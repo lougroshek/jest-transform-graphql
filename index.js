a5214b5e70eaeca2ab25fc9bd4032905284e32b5
Alex Caza <alex@alexcaza.com>
2022-05-17 13:56:41 -0400

    Fix index.js issue

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const loader = require('graphql-tag/loader');
+const loader = require("graphql-tag/loader");
 
 module.exports = {
   process(src) {
-    // call directly the webpack loader with a mocked context 
+    // call directly the webpack loader with a mocked context
     // as graphql-tag/loader leverages `this.cacheable()`
     return {
-      code: loader.call({ cacheable() {} }, src
+      code: loader.call({ cacheable() {} }, src),
     };
   },
 };
